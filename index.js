74b21126244bad84560109b0591ca2ea052dcbd3
Lizzie <LaShark@LaShark.local>
2016-04-13 11:11:58 +1200

    set up and trying out main functions

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ var Promise = require('promise');
 var request = require('superagent-promise')(require('superagent'), Promise);
 var fs = require('fs')
 var cheerio = require('cheerio')
+var $ = require('jquery')
+
+//http://www.parliament.nz/en-nz/syndication?posting=/en-nz/pb/sc/make-submission/
+//http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+//http://doc.scrapy.org/en/0.14/intro/overview.html
+//http://www.parliament.nz/en-nz/pb/sc/make-submission?Criteria.PageNumber=1&Criteria.ViewDetails=1
+//http://www.parliament.nz/en-nz/pb/sc/make-submission
+
+$(document).ready(function() {
 
 
-request.get('http://www.parliament.nz/en-nz/pb/sc/make-submission') //gets the website using superagent
+  request.get('http://www.parliament.nz/en-nz/pb/sc/make-submission') //gets the website using superagent
   .then(function(results){
       var $ = cheerio.load(results.text)
       var links = []
@@ -12,8 +21,18 @@ request.get('http://www.parliament.nz/en-nz/pb/sc/make-submission') //gets the w
         links[i] = $(this).attr('href')     //cheerio documentation pushing all 'a' things with href
                                             //attributes into the previously declared links array
   })
+
     console.log(links)
   })
   .catch(function(err){
     console.log("Uh ooh...")
   })
+
+  var extractSubs = function() {
+    var subLinks = []
+    subLinks.push($("links:contains(make-submission)"))
+  }
+
+  extractSubs();
+
+  });
