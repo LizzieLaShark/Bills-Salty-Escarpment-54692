fe0cdc9147e3b4d447dc98cf39f18f8b69026052
Lizzie <LaShark@LaShark.local>
2016-04-13 18:30:00 +1200

    scraped links, stored as json in json file. Still need to filter and turn from relative to absolute

--- index.js
+++ index.js
@@ -4,35 +4,37 @@ var fs = require('fs')
 var cheerio = require('cheerio')
 var $ = require('jquery')
 
-//http://www.parliament.nz/en-nz/syndication?posting=/en-nz/pb/sc/make-submission/
-//http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
-//http://doc.scrapy.org/en/0.14/intro/overview.html
-//http://www.parliament.nz/en-nz/pb/sc/make-submission?Criteria.PageNumber=1&Criteria.ViewDetails=1
-//http://www.parliament.nz/en-nz/pb/sc/make-submission
-
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-  request.get('http://www.parliament.nz/en-nz/pb/sc/make-submission') //gets the website using superagent
+
+request.get('http://www.parliament.nz/en-nz/pb/sc/make-submission')  //gets the website using superagent
   .then(function(results){
       var $ = cheerio.load(results.text)
-      var links = []
+      var linksArray = []
       $('a').each(function(i, elem) {       //special cheerio documentation that is getting links
-        links[i] = $(this).attr('href')     //cheerio documentation pushing all 'a' things with href
-                                            //attributes into the previously declared links array
-  })
+        linksArray[i] = $(this).attr('href')     //cheerio documentation pushing all 'a' things with href
+      })                                    //attributes into the previously declared links array
 
-    console.log(links)
+    console.log(linksArray)
+    return linksArray
+
+  })
+  .then(function(results){
+     fs.writeFile('./links.json', JSON.stringify(results), function(err, data) {
+        if (err) console.log(err);
+     })
   })
   .catch(function(err){
     console.log("Uh ooh...")
   })
 
-  var extractSubs = function() {
-    var subLinks = []
-    subLinks.push($("links:contains(make-submission)"))
-  }
 
-  extractSubs();
 
-  });
+
+
+//   // var jsonLinks = JSON.parse(linksArray)
+//   // console.log(jsonLinks)
+//
+//
+//   // module.exports = scrapes
---
+++ scraper.js
@@ -0,0 +1,6 @@
+var scrapes = require('/Users/LaShark/Desktop/Phase2PersonalProject/index.js')
+
+
+var jsonLinks = JSON.parse(linksArray)
+
+console.log(jsonLinks)
