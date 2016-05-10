d29650cb10fd18dc27087ced9be294e377102575
Lizzie LaShark <email": null>
2016-05-10 16:07:04 +1200

    db refreshing data and scraper.js scraping only relevant title links

--- scraper.js
+++ scraper.js
@@ -24,30 +24,24 @@ var scraper = function(){
          url: $(this).attr('href')
         }
       })
-
-    // console.log(linksArray)
     return linksArray
-
   })
   .then(function(results){
-    //func below will need to be changed. When more than this amount of subs are up they wont display.
-    //Need to find a way to knowck off the last 9 and the firs 35 while haing the middle
-    //fluctuate depending on # of subs
-      var sliced = results.slice(35, 51)
-      console.log("we're scrapin and slicin!")
-      //loop over sliced, for each title in sliced, insert into db.
+      var sliced = results.slice(35, -9)
+      console.log(sliced)
+
+      knex('submissions').del().then(function(){
+          console.log("ran the deletions function")
+      })
       knex('submissions').insert(sliced).then(function(){
         console.log("done")
       })
-
-      //  fs.writeFile('./links.json', JSON.stringify(sliced), function(err, data) {
-      //     if (err) console.log(err);
-      //  })
   })
   .catch(function(err){
     console.log(err)
   })
 }
+
 
 scraper();
 
